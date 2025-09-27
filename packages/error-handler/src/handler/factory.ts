87a6bf1b8eae3216bb17421c2a149aa06be9a7f6
khaled <khaledeskandrany@gmail.com>
2025-09-27 18:30:20 +0300

    refactor(error-handler/zod): upgrand zod to 4.0.5 in all apps and packages and remove zod classes from error-handler

--- packages/error-handler/src/handler/factory.ts
+++ packages/error-handler/src/handler/factory.ts
@@ -1,17 +1,15 @@
 import { ILogger } from "../errors/types.js";
 import { HttpErrorSerializer } from "../serializers/HttpErrorSerializer.js";
-import { ZodErrorSerializer } from "../serializers/ZodErrorSerializer.js";
+
 import { ErrorHandler } from "./ErrorHandlerContext.js";
 import { FallbackErrorStrategy } from "./FallBackErrorHandlerStrategy.js";
 import { HttpErrorHandlerStrategy } from "./HttpErrorHandlerStrategy.js";
 import { InputValidationErrorHandlerStrategy } from "./InputValidationErrorHandlerStrategy.js";
 
-import { ZodErrorhandlerStrategy } from "./ZodErrorHandlerStrategy.js";
-
 export const createErrHandlerMiddleware = (logger?: ILogger) =>
   new ErrorHandler([
     new HttpErrorHandlerStrategy(new HttpErrorSerializer(), logger),
     new InputValidationErrorHandlerStrategy(logger),
-    new ZodErrorhandlerStrategy(new ZodErrorSerializer(), logger),
+
     new FallbackErrorStrategy(logger),
   ]).handle;
